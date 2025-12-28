c6a0151a57e966d87242d8fc6f85df56b5d29c38
bodeulbodeul <ysymfw@gmail.com>
2025-12-28 11:14:09 +0900

    feat: PDF 문서 로딩 기능 구현 및 텍스트 공백 전처리 적용

--- src/main/java/com/example/restservice/DataLoader.java
+++ src/main/java/com/example/restservice/DataLoader.java
@@ -3,12 +3,15 @@ package com.example.restservice;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -17,20 +20,33 @@ public class DataLoader {
     private final VectorStore vectorStore;
 
     @Value("classpath:regulations.txt")
-    private Resource requlation;
+    private Resource textResource;
+
+    @Value("classpath:rule.pdf")
+    private Resource pdfResource;
 
     public DataLoader(VectorStore vectorStore) {
         this.vectorStore = vectorStore;
     }
 
     @PostConstruct
-    public void load() {
+    public void init() {
+        loadText();
+        loadPdf();
+    }
+
+    public void loadText() {
+
+        if (!textResource.exists()) {
+            System.out.println("text 규정집이 없습니다.");
+            return;
+        }
 
         // 테스트 파일 읽기
-        TextReader textReader = new TextReader(requlation);
+        TextReader textReader = new TextReader(textResource);
 
         // 메타데이터 추가
-        textReader.getCustomMetadata().put("filename", "regulations.txt");
+        textReader.getCustomMetadata().put("filename", textResource.getFilename());
 
         List<Document> documents = textReader.get();
 
@@ -39,7 +55,39 @@ public class DataLoader {
         List<Document> splitDocuments = splitter.apply(documents);
 
         // 저장소에 데이터 적재 (비용발생)
-        vectorStore.add(documents);
-        System.out.println("✅ 'regulations.txt' 파일 내용을 성공적으로 학습했습니다!");
+        vectorStore.add(splitDocuments);
+        System.out.println(textResource.getFilename() + "파일 내용을 성공적으로 학습했습니다!");
+    }
+
+    public void loadPdf() {
+        if (!pdfResource.exists()) {
+            System.out.println("PDF 규정집이 없습니다.");
+            return;
+        }
+
+        // PDF 파일 읽기
+        PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(
+                pdfResource, PdfDocumentReaderConfig.builder().withPagesPerDocument(1).build()
+        );
+
+        List<Document> cleanDocuments = new ArrayList<>();
+
+        // 메타데이터 추가
+        for (Document doc : pdfReader.get()) {
+            doc.getMetadata().put("filename", pdfResource.getFilename());
+
+            String content = doc.getContent();
+            String cleanContent = content.replaceAll("\\s+", " ").trim();
+
+            Document newDoc = new Document(cleanContent, doc.getMetadata());
+
+            cleanDocuments.add(newDoc);
+        }
+
+        TokenTextSplitter splitter = new TokenTextSplitter();
+        List<Document> splitDocuments = splitter.apply(cleanDocuments);
+
+        vectorStore.add(splitDocuments);
+        System.out.println(pdfResource.getFilename() + "파일 내용을 성공적으로 학습했습니다!");
     }
 }
