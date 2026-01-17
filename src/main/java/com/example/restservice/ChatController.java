fbaa277694492db4d0885a853a2904bec66c8431
bodeulbodeul <ysymfw@gmail.com>
2026-01-17 10:45:01 +0900

    feat: RAG 검색 임계값 설정 및 출처 표기 로직 개선

--- src/main/java/com/example/restservice/ChatController.java
+++ src/main/java/com/example/restservice/ChatController.java
@@ -1,14 +1,15 @@
 
 package com.example.restservice;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,29 +32,46 @@ public class ChatController {
     private Resource ragPrompt;
 
     public ChatController(ChatClient.Builder builder, VectorStore vectorStore) {
-        this.chatClient = builder.defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
-                .build();
         this.vectorStore = vectorStore;
+        this.chatClient = builder
+                .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
+                .build();
     }
 
     @GetMapping("/ai/chat")
-    public String chat(@RequestParam(value = "message") String message) {
+    public String chat(@RequestParam("message") String message, @RequestParam(value = "filename", required = false) String filename) {
+
+        // 검색 조건 설정
+        SearchRequest searchRequest = SearchRequest
+                .query(message)
+                .withTopK(3) // 개수
+                .withSimilarityThreshold(0.2); // 유사도
+
+        if (StringUtils.isNotBlank(filename)) {
+            searchRequest.withFilterExpression(
+                    new FilterExpressionBuilder()
+                            .eq("filename", filename)
+                            .build()
+            );
+        }
+
         // 사용자 질문 벡터 검색
-        List<Document> foundDocs = vectorStore.similaritySearch(SearchRequest.query(message).withTopK(2));
+        List<Document> foundDocs = vectorStore.similaritySearch(searchRequest);
 
-        // 답변 데이터
-        String context = foundDocs.stream().map(Document::getContent).collect(Collectors.joining("\n\n---\n\n"));
-        // 참조 규정집 목록
-        String sourceList = foundDocs.stream()
-                .map(doc -> (String) doc.getMetadata().getOrDefault("filename", "unknown"))
-                .distinct()
-                .collect(Collectors.joining(", "));
+        String context = foundDocs.stream()
+                .map(doc -> {
+                    String fileName = (String) doc.getMetadata().getOrDefault("filename", "참고문서");
+                    return String.format("""
+                            [문서: %s]
+                            %s
+                            """, fileName, doc.getContent());
+                }).collect(Collectors.joining("\n\n---\n\n"));
 
+        // 프롬프트 렌더링
         PromptTemplate promptTemplate = new PromptTemplate(ragPrompt);
         String promptText = promptTemplate.render(Map.of(
                 "context", context,
-                "message", message,
-                "source_list", sourceList
+                "message", message
         ));
 
         return chatClient.prompt()
